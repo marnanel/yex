5655e3a878a9af184c81b00f26a9c37760f24f27
Marnanel Thurman <marnanel@thurman.org.uk>
2021-12-26 21:29:22 +0000

    first attempt at test_token. Only tests Token so far, not yet Tokeniser.

--- test/test_token.py
+++ test/test_token.py
@@ -1,1 +1,34 @@
-pass
+from mex.token import Token
+
+def test_token_simple_create():
+    t = Token('q', 0)
+    assert t is not None
+
+def test_token_cats():
+
+    categories = [
+            'Escape character',
+            'Beginning of group',
+            'End of group',
+            'Math shift',
+            'Alignment tab',
+            'End of line',
+            'Parameter',
+            'Superscript',
+            'Subscript',
+            'Ignored character',
+            'Space',
+            'Letter',
+            'Other character',
+            'Active character',
+            'Comment character',
+            'Invalid character',
+            ]
+
+    for i in range(16):
+        t = Token(
+                ch = chr(i+32),
+                category = i)
+        assert ord(t.ch) == i+32
+        assert t.category == i
+        assert t.meaning == categories[i]
