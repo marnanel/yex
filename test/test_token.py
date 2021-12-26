268117119d5229aa538b83f201ffb7970287f6c4
Marnanel Thurman <marnanel@thurman.org.uk>
2021-12-26 21:46:57 +0000

    First tests for Tokeniser, which demonstrate it doesn't work very well yet

--- test/test_token.py
+++ test/test_token.py
@@ -1,4 +1,6 @@
-from mex.token import Token
+import io
+from mex.state import State
+from mex.token import Token, Tokeniser
 
 def test_token_simple_create():
     t = Token('q', 0)
@@ -32,3 +34,26 @@ def test_token_cats():
         assert ord(t.ch) == i+32
         assert t.category == i
         assert t.meaning == categories[i]
+
+def test_tokeniser_simple_create():
+    s = State()
+    t = Tokeniser(s)
+    assert t is not None
+
+def test_tokeniser_simple_text():
+    s = State()
+    t = Tokeniser(s)
+
+    result = [
+            ]
+
+    with io.StringIO("""
+    fred
+    """) as f:
+
+        for item in t.read(f):
+            print(9, item)
+            result.append(item)
+
+    print(result)
+    assert False
