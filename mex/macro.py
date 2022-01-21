cdfd1a2ceffb1e262ff17e057fc17b3f8ec0322d
Marnanel Thurman <marnanel@thurman.org.uk>
2022-01-21 18:41:33 +0000

    Basic macros; params not yet implemented

--- mex/macro.py
+++ mex/macro.py
@@ -1,5 +1,7 @@
 class Macro:
 
+    magic = False
+
     @property
     def name(self):
         return self.__class__.__name__.lower()
@@ -10,8 +12,19 @@ class Macro:
     def get_params(self, tokeniser, tokens):
         raise ValueError("superclass does nothing useful in itself")
 
-    def __call__(self):
+    def __call__(self, tokens):
         raise ValueError("superclass does nothing useful in itself")
+
+class _UserDefined(Macro):
+
+    def __init__(self,
+            definition):
+
+        # TODO anything about params
+        self.definition = definition
+
+    def __call__(self, state, tokens):
+        return self.definition
 
 class Catcode(Macro):
 
@@ -19,8 +32,80 @@ class Catcode(Macro):
         n = Number(tokeniser, tokens)
         raise KeyError(n)
 
-    def __call__(self):
+    def __call__(self, state, tokens):
         raise ValueError("catcode called")
+
+class Def(Macro):
+
+    def get_params(self, tokeniser, tokens):
+        n = Number(tokeniser, tokens)
+        raise KeyError(n)
+
+    def __call__(self, state, tokens):
+        print('def here!')
+
+        for token in tokens:
+            if token.category != token.CONTROL:
+                raise ValueError(
+                        f"\\def must be followed by a control sequence")
+            macro_name = token.name
+            break
+
+        # now parameters
+        for token in tokens:
+            if token.category == token.BEGINNING_GROUP:
+                break
+            else:
+                raise ValueError("Please implement parameters")
+
+        # now the definition
+        grouping_level = 1
+        definition = []
+
+        for token in tokens:
+            if token.category == token.BEGINNING_GROUP:
+                grouping_level += 1
+            elif token.category == token.END_GROUP:
+                grouping_level -= 1
+                if grouping_level == 0:
+                    break
+
+            definition.append(token)
+
+        new_macro = _UserDefined(
+                definition = definition,
+                )
+
+        state[f'macro {macro_name}'] = new_macro
+
+        # a definition produces no output of its own
+        return []
+
+class Expander:
+
+    def __init__(self, tokeniser):
+        self.tokeniser = tokeniser
+        self.state = tokeniser.state
+
+        if 'macros' not in self.state.values[-1]:
+            add_macros_to_state(self.state)
+
+    def read(self, f):
+
+        tokens = self.tokeniser.read(f)
+        for token in tokens:
+
+            if token.category!=token.CONTROL:
+                yield token
+                continue
+
+            handler = self.state[f'macro {token.name}']
+
+            if handler is None:
+                raise KeyError(f"there is no macro called {token.name}")
+
+            for item in handler(state=self.state, tokens=tokens):
+                yield item
 
 def add_macros_to_state(state):
     state.add_state(
@@ -31,11 +116,12 @@ def add_macros_to_state(state):
 def names():
 
     result = dict([
-            (name.lower(), value) for
+            (name.lower(), value()) for
             (name, value) in globals().items()
             if value.__class__==type and
             value!=Macro and
-            issubclass(value, Macro)
+            issubclass(value, Macro) and
+            not name.startswith('_')
             ])
 
     return result
--- test/test_macro.py
+++ test/test_macro.py
@@ -1,6 +1,7 @@
 import io
 from mex.state import State
-from mex.macro import add_macros_to_state
+from mex.macro import add_macros_to_state, Expander
+from mex.token import Tokeniser
 
 def test_add_macros_to_state():
     s = State()
@@ -8,3 +9,31 @@ def test_add_macros_to_state():
 
     add_macros_to_state(s)
     assert s['macro catcode'] is not None
+
+def test_expand_simple():
+    s = State()
+    t = Tokeniser(s)
+    e = Expander(t)
+
+    string = "This is a test"
+
+    with io.StringIO(string) as f:
+        result = ''.join([
+            t.ch for t in e.read(f)
+            ])
+
+    assert(result==string)
+
+def test_expand_simple_def():
+    s = State()
+    t = Tokeniser(s)
+    e = Expander(t)
+
+    string = "\\def\\wombat{Wombat}\\wombat"
+
+    with io.StringIO(string) as f:
+        result = ''.join([
+            t.ch for t in e.read(f)
+            ])
+
+    assert(result=="Wombat")
