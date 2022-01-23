cf8344b3e6c25b2cf61fa47b9e43f6a50fa42052
Marnanel Thurman <marnanel@thurman.org.uk>
2022-01-23 00:13:25 +0000

    Rearranged put() so it uses a helper function to handle both str and file, rather than recurring, which might fail and crash

--- mex/put.py
+++ mex/put.py
@@ -2,26 +2,31 @@ import io
 import mex.state
 import mex.token
 
-def put(text):
+def _put_from_file(source):
 
     state = mex.state.State()
     result = ''
 
-    with io.StringIO(text) as f:
+    tokens = mex.token.Tokeniser(
+            state = state,
+            source = source,
+            )
 
-        tokens = mex.token.Tokeniser(
-                state = state,
-                source = f,
-                )
+    for item in tokens:
 
-        for item in tokens:
-
-            if item.category in (item.LETTER, item.SPACE, item.OTHER):
-                result += item.ch
-            else:
-                raise ValueError(f"Don't know category for {item}")
+        if item.category in (item.LETTER, item.SPACE, item.OTHER):
+            result += item.ch
+        else:
+            raise ValueError(f"Don't know category for {item}")
 
     return result
+
+def put(source):
+    if hasattr(source, 'read'):
+        return _put_from_file(source)
+    else:
+        with io.StringIO(str(source)) as f:
+            return _put_from_file(f)
 
 if __name__=='__main__':
     print(put("If you see this, it works."))
