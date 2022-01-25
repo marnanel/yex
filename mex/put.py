d2c59425416773e396069f65d950fd8436db8cd1
Marnanel Thurman <marnanel@thurman.org.uk>
2022-01-25 00:03:41 +0000

    mex.put now has a basic but proper CLI front end with argparse and so on

--- mex/put.py
+++ mex/put.py
@@ -1,20 +1,23 @@
 import io
 import mex.state
 import mex.token
+import mex.macro
+import argparse
 
 def _put_from_file(source):
 
     state = mex.state.State()
     result = ''
 
-    tokens = mex.token.Tokeniser(
-            state = state,
-            source = source,
-            )
+    for item in mex.macro.Expander(
+            mex.token.Tokeniser(
+                state = state,
+                source = source,
+                )):
+        print(item)
 
-    for item in tokens:
-
-        if item.category in (item.LETTER, item.SPACE, item.OTHER):
+        if item.category in (item.LETTER, item.SPACE,
+                item.OTHER, item.END_OF_LINE):
             result += item.ch
         else:
             raise ValueError(f"Don't know category for {item}")
@@ -28,5 +31,17 @@ def put(source):
         with io.StringIO(str(source)) as f:
             return _put_from_file(f)
 
+def main():
+
+    parser = argparse.ArgumentParser(
+            description='beautiful typesetting',
+            )
+    parser.add_argument('filename', type=str,
+        help='file to process')
+
+    args = parser.parse_args()
+    with open(args.filename, 'r') as f:
+        put(f)
+
 if __name__=='__main__':
-    print(put("If you see this, it works."))
+    main()
