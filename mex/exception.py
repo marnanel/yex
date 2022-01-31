9d1940ef618c02ac63fbf68a4946e2d744cc725b
Marnanel Thurman <marnanel@thurman.org.uk>
2022-01-31 19:10:46 +0000

    MexError superclass. ParseError.__str__ -> __repr__ so it prints properly in tests.

--- mex/exception.py
+++ mex/exception.py
@@ -1,10 +1,13 @@
-class ParseError(Exception):
+class MexError(Exception):
+    pass
+
+class ParseError(MexError):
 
     def __init__(self, message, tokeniser):
         self.message = message
         self.tokeniser = tokeniser
 
-    def __str__(self):
+    def __repr__(self):
         return self.tokeniser.error_position(
                 message = self.message,
                 )
