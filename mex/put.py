75a0214d5d3e58ed3cce24a21d28b1b7e7e1d1e6
Marnanel Thurman <marnanel@thurman.org.uk>
2022-02-08 17:25:17 +0000

    put() checks whether a control is a variable using duck typing

--- mex/put.py
+++ mex/put.py
@@ -22,13 +22,15 @@ def _put_from_file(source,
                 ))
     for item in e:
         macro_logger.debug("  -- resulting in: %s", str(item))
-        if isinstance(item, mex.state.Variable):
+
+        try:
             item.set_from_tokens(e)
-        elif item.category in (item.LETTER, item.SPACE,
-                item.OTHER, item.END_OF_LINE):
-            result += item.ch
-        else:
-            raise ValueError(f"Don't know category for {item}")
+        except AttributeError:
+            if item.category in (item.LETTER, item.SPACE,
+                    item.OTHER, item.END_OF_LINE):
+                result += item.ch
+            else:
+                raise ValueError(f"Don't know category for {item}")
 
     return result
 
