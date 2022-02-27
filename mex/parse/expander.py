9ca32c0535cd6aeda74189548a930008b1b9f49d
Marnanel Thurman <marnanel@thurman.org.uk>
2022-02-27 14:54:55 +0000

    "outer" controls and active characters cause errors even if they're in places we're not running (e.g. because of conditionals), *except* if we're running a string macro such as \message{}.

--- mex/parse/expander.py
+++ mex/parse/expander.py
@@ -68,16 +68,11 @@ class Expander:
 
         while True:
 
-            try:
-                token = self.tokens.__next__()
-            except StopIteration:
+            for token in self.tokens:
+                break
 
-                if self.tokens.push_back:
-                    token = self.tokens.push_back.pop()
-                elif self.allow_eof:
-                    return
-                else:
-                    token = None
+            if token is None and self.allow_eof:
+                return
 
             macros_logger.debug("token: %s", token)
 
@@ -86,6 +81,7 @@ class Expander:
                     raise mex.exception.ParseError(
                             "runaway expansion")
 
+            macros_logger.debug("single_grouping is %d", self.single_grouping)
             if self.single:
 
                 if self.single_grouping==-1:
@@ -106,27 +102,15 @@ class Expander:
                     # so we yield that and then stop.
                     macros_logger.debug("  -- the only symbol in a single")
                     self.single_grouping = -1
+                    macros_logger.debug("single_grouping now %d", self.single_grouping)
                 elif token.category==token.END_GROUP:
                     self.single_grouping -= 1
                     if self.single_grouping==0:
                         macros_logger.debug("  -- the last } in a single")
                         return
 
-            if not self.running:
+            if token is None:
                 yield token
-
-            elif token is None:
-                yield token
-
-            elif token.category==token.BEGINNING_GROUP:
-                self.state.begin_group()
-
-            elif token.category==token.END_GROUP:
-                try:
-                    self.state.end_group()
-                except ValueError as ve:
-                    raise mex.exception.ParseError(
-                            str(ve))
 
             elif token.category in [token.CONTROL, token.ACTIVE]:
 
@@ -153,6 +137,18 @@ class Expander:
                     raise mex.exception.MacroError(
                             "outer macro called where it shouldn't be")
 
+                elif not self.running and not isinstance(
+                        handler, mex.control.C_StringMacro):
+                    # don't refactor this into the other "not running";
+                    # if it's a control or active character, we must
+                    # raise an error if it's "outer", even if we're
+                    # not running.
+                    macros_logger.debug(
+                            "%s: we're not running; yielding control token",
+                            token)
+
+                    yield token
+
                 elif self.state.ifdepth[-1] or isinstance(
                         handler, mex.control.C_StringMacro):
                     # We're not prevented from executing by \if.
@@ -168,6 +164,11 @@ class Expander:
 
                     # control exists, so run it.
                     if isinstance(handler, mex.control.C_StringMacro):
+                        commands_logger.debug(
+                                "%s:   -- special case, string macro",
+                                handler,
+                                )
+
                         handler_result = handler(
                                 name = token,
                                 tokens = self.tokens,
@@ -191,6 +192,23 @@ class Expander:
                     commands_logger.debug("Not executing %s because "+\
                             "we're inside a conditional block",
                             handler)
+
+            elif not self.running:
+                macros_logger.debug(
+                        "%s: we're not running; yielding it",
+                        token)
+
+                yield token
+
+            elif token.category==token.BEGINNING_GROUP:
+                self.state.begin_group()
+
+            elif token.category==token.END_GROUP:
+                try:
+                    self.state.end_group()
+                except ValueError as ve:
+                    raise mex.exception.ParseError(
+                            str(ve))
 
             elif self.state.ifdepth[-1]:
                 yield token
