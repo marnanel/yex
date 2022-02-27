2d3b822d1e7fef80edb0aa73205be0eedc0e6d04
Marnanel Thurman <marnanel@thurman.org.uk>
2022-02-27 23:39:57 +0000

    mex.control.__init__ takes its __all__ from introspection

--- mex/control/__init__.py
+++ mex/control/__init__.py
@@ -8,82 +8,25 @@ from mex.control.arithmetic import *
 from mex.control.io import *
 from mex.control.other import *
 
-__all__ = [
-        'ControlsTable',
-        'ControlWord',
+g = list(globals().items())
 
-        'C_ControlWord',
-        'C_UserDefined',
-        'Def',
-        'Outer',
-        'Gdef',
-        'Outer',
-        'Long',
-        'Edef',
-        'Xdef',
-        'Global',
-        'C_Defined',
-        'Chardef',
-        'Mathchardef',
-        'Par',
-        'C_StringControl',
-        'Message',
-        'Errmessage',
-        'Special',
-        'Countdef',
-        'Dimendef',
-        'Skipdef',
-        'Muskipdef',
-        'Toksdef',
-        'Advance',
-        'Multiply',
-        'Divide',
-        'The',
-        'Let',
-        'Font',
-        'Relax',
-        'Hbox',
-        'Vbox',
-        'Noindent',
-        'Indent',
-
-        'C_Conditional',
-        'Iftrue',
-        'Iffalse',
-        'Ifnum',
-        'Ifdim',
-        'Ifodd',
-        '_Ifmode',
-        'Ifvmode',
-        'Ifhmode',
-        'Ifmmode',
-        'Ifinner',
-        'If',
-        'Ifcat',
-        'Fi',
-        'Else',
-        'Ifcase',
-        'Or',
-
-        'Immediate',
-
-        'Noexpand',
-        'Showlists',
-                ]
+__all__ = list([
+    name for name, value in g
+    if value.__class__==type and
+    issubclass(value, C_ControlWord)
+    ])
 
 def handlers():
 
     # Take a copy. Sometimes evaluating a macro may
     # create another macro, which changes the size
     # of globals().items() and confuses the list comprehension.
-    g = list(globals().items())
 
     result = dict([
             (name.lower(), value()) for
             (name, value) in g
             if value.__class__==type and
-            value!=mex.control.word.C_ControlWord and
-            issubclass(value, mex.control.word.C_ControlWord) and
+            issubclass(value, C_ControlWord) and
             not name.startswith('C_')
             ])
 
