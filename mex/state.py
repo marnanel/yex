marnanel/yex

commit 419502a7aca0b90f9d9a161b06722ca88f5be84f

State.mode returns the actual Mode object

mex/state.py
@@ -240,4 +240,4 @@ class State:
 
     @property
     def mode(self):
-        return self.controls['_mode'].mode
+        return self.controls['_mode'].value

commit acf42821c6cefd7d61dfd7db0475ccefac2d396d

State.mode returns the actual mode. Undoes commit 419502a7 which broke it.

mex/state.py
@@ -240,4 +240,4 @@ class State:
 
     @property
     def mode(self):
-        return self.controls['_mode'].value
+        return self.controls['_mode'].mode