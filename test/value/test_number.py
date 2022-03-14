6955d6ba99f68d2228e82b63c5870ad7fbe9871e
Marnanel Thurman <marnanel@thurman.org.uk>
2022-03-14 13:51:53 +0000

    fixed test_number_from_count
    if you're constructing a Number, you can't supply "\the..." because
    that's not a Number, that's a command

--- test/value/test_number.py
+++ test/value/test_number.py
@@ -189,7 +189,7 @@ def test_number_from_count():
     state = State()
     state['count1'] = 100
 
-    with expander_on_string(r'\the\count1', state) as t:
+    with expander_on_string(r'\count1', state) as t:
         n = Number(t)
 
     assert n==100
