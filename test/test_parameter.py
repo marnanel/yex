c75434cfc0cec68b72fb5c7ce7f2daf951b18bc2
Marnanel Thurman <marnanel@thurman.org.uk>
2022-03-14 03:16:31 +0000

    Parameter tests pass

--- test/test_parameter.py
+++ test/test_parameter.py
@@ -8,20 +8,23 @@ def test_parameter_getting():
             mode='vertical',
             call=r"\the\defaulthyphenchar",
             state=s,
+            find='chars',
             )=='100'
 
 def test_parameter_setting():
     s = State()
     s['defaulthyphenchar'] = 100
-    assert call_macro(
+    assert run_code(
             call=r"\defaulthyphenchar 90",
             state=s,
+            find='chars',
             )==''
     assert s['defaulthyphenchar'].value == 90
 
     s['defaulthyphenchar'] = '?'
-    assert call_macro(
+    assert run_code(
             call=r"\defaulthyphenchar = 90",
             state=s,
+            find='chars',
             )==''
     assert s['defaulthyphenchar'].value == 90
