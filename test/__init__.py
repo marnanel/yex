marnanel/yex

commit 705dfb0ba68f1d65213c8c0088b4bcce4a08c6ff

Added call stack and call frames; Expander.next() takes more parameters.
A call frame is added to State.call_stack when every macro begins
executing, and is removed from State.call_stack when it stops.
(If they're interleaved, it doesn't cause errors, but the ordering
on the stack may get confused.)

This has meant adding a new kind of Token called Internal, with
category==Token.INTERNAL. The cless is not exported from mex.parse.
When the expander sees an Internal token, it executes it.

Also, Expander.next() takes no_par and no_outer arguments.
The process of restoring Expander's attributes at the end of
its next() method has been generalised, and is now less hacky.

Tests updated. And some docstrings and comments added.

mex/control/conditional.py
@@ -154,20 +154,23 @@ class _If_or_Ifcat(C_Conditional):
 
         comparands = []
 
-        for t in tokens.child(
+        left = tokens.next(
                 no_outer=True,
-                ):
-            comparands.append(t)
-            if len(comparands)>1:
-                break
+                expand=True,
+                )
+        right = tokens.next(
+                no_outer=True,
+                expand=True,
+                )
 
         commands_logger.debug(
-                r"\%s %s",
+                r"\%s: %s, %s ",
                 self.__class__.__name__.lower(),
-                comparands)
+                left, right,
+                )
 
-        if self.get_field(comparands[0])==\
-                self.get_field(comparands[1]):
+        if self.get_field(left)==\
+                self.get_field(right):
             self._do_true(tokens.state)
         else:
             self._do_false(tokens.state)

mex/control/macro.py
@@ -4,12 +4,53 @@ import mex.parse
 import mex.value
 import mex.exception
 import mex.font
+import mex.state
 import string
 
 macros_logger = logging.getLogger('mex.macros')
 commands_logger = logging.getLogger('mex.commands')
 
+class _Store_Call(mex.parse.token.Internal):
+    """
+    Pushes a call frame onto the call stack.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args)
+        self.record = mex.state.Callframe(
+                **kwargs,
+                )
+
+    def __call__(self, name, tokens):
+        tokens.state.call_stack.append(self.record)
+        with open('/tmp/args.txt', 'a') as f:
+            f.write(repr(self.record)+'\n')
+
+        macros_logger.debug(
+                "call stack: push: %s",
+                tokens.state.call_stack)
+
+    def __repr__(self):
+        return f'[call: {self.record}]'
+
+class _Store_Return(mex.parse.token.Internal):
+    """
+    Pops a frame from the call stack.
+    """
+
+    def __call__(self, name, tokens):
+        tokens.state.call_stack.pop()
+        macros_logger.debug(
+                "call stack: pop:  %s",
+                tokens.state.call_stack)
+
+    def __repr__(self):
+        return f'[return]'
+
 class C_Macro(C_Defined):
+    r"""
+    Any macro defined using \def.
+    """
 
     def __init__(self,
             definition,
@@ -29,7 +70,15 @@ class C_Macro(C_Defined):
         interpolated = self._part2_interpolate(arguments)
         macros_logger.debug('%s: result=%s', name, interpolated)
 
+        # Push store and return back to front, because these tokens
+        # are retrieved first-in-first-out.
+        tokens.push(_Store_Return())
         tokens.push(interpolated)
+        tokens.push(_Store_Call(
+            callee = name,
+            args = arguments,
+            location = name.location,
+            ))
 
     def _part1_find_arguments(self, name, tokens):
 
@@ -189,7 +238,7 @@ class C_Macro(C_Defined):
                     interpolated.extend(
                             arguments[int(t.ch)-1],
                             )
-            elif t.category==t.PARAMETER:
+            elif isinstance(t, mex.parse.Token) and t.category==t.PARAMETER:
                 find_which_param = True
             else:
                 interpolated.append(t)

mex/parse/expander.py
@@ -155,7 +155,7 @@ class InfiniteExpander(Tokenstream):
                     raise mex.exception.ParseError(
                             "runaway expansion")
 
-            if self.single:
+            if self.single and token.category!=token.INTERNAL:
 
                 if self.single_grouping==-1:
                     # self.single was set, and the first token wasn't
@@ -281,6 +281,13 @@ class InfiniteExpander(Tokenstream):
                             handler,
                             )
 
+            elif token.category==token.INTERNAL:
+                commands_logger.debug("%s:  -- running internal token: %s",
+                        self,
+                        token,
+                        )
+                token(token, self)
+
             elif not self.expand:
                 macros_logger.debug(
                         "%s: we're not expanding; returning %s",
@@ -302,7 +309,8 @@ class InfiniteExpander(Tokenstream):
                         )
                 return token
             else:
-                commands_logger.debug("%s:  -- dropping because of conditional: %s",
+                commands_logger.debug(
+                        "%s:  -- dropping because of conditional: %s",
                         self,
                         token,
                         )
@@ -349,9 +357,11 @@ class InfiniteExpander(Tokenstream):
     def next(self,
             expand = None,
             on_eof = None,
+            no_outer = None,
+            no_par = None,
             deep = False,
             ):
-        """
+        r"""
         Returns a single token, just like next()
         on an iterator, but with more options:
 
@@ -363,6 +373,8 @@ class InfiniteExpander(Tokenstream):
                         EOF_EXHAUST is treated like EOF_RETURN_NONE.
                         If unspecified, go with the defaults for
                         this InfiniteExpander.
+        no_par -        if True, finding "\par" will cause an error.
+        no_outer -      if True, finding an outer macro will cause an error.
         deep -          If True, the token is taken from
                         the tokeniser rather than the Expander.
                         This allows you to see characters that
@@ -371,15 +383,15 @@ class InfiniteExpander(Tokenstream):
                         This option ignores the value of expand,
                         because a tokeniser doesn't expand anyway.
         """
-        # FIXME the save/restore stuff in here is hacky
 
-        if expand is not None:
-            was_expand = self.expand
-            self.expand = expand
+        restore = {}
 
-        if on_eof is not None:
-            was_on_eof = self.on_eof
-            self.on_eof = on_eof
+        for field in ['expand', 'on_eof', 'no_outer', 'no_par']:
+            whether = locals()[field]
+            if whether is None:
+                continue
+            restore[field] = getattr(self, field)
+            setattr(self, field, whether)
 
         if deep:
             result = next(self.tokeniser)
@@ -387,13 +399,12 @@ class InfiniteExpander(Tokenstream):
             result = self._read()
 
         if result is None and self.on_eof==self.EOF_RAISE_EXCEPTION:
+            # This is usually already caught, but might not have been
+            # if deep=True
             raise mex.exception.ParseError("unexpected EOF")
 
-        if expand is not None:
-            self.expand = was_expand
-
-        if on_eof is not None:
-            self.on_eof = was_on_eof
+        for f,v in restore.items():
+            setattr(self, f, v)
 
         return result
 

mex/parse/token.py
@@ -22,7 +22,8 @@ class Token:
     COMMENT = 14
     INVALID = 15
 
-    CONTROL = {}
+    CONTROL = 'c'
+    INTERNAL = 'i'
 
     def __init__(self,
             ch,
@@ -85,6 +86,8 @@ class Token:
             return 'Invalid character'
         elif self.category==self.CONTROL:
             return 'Control'
+        elif self.category==self.INTERNAL:
+            return 'Internal'
         else:
             raise ValueError(
                     f"impossible: category {self.category} does not exist")
@@ -165,6 +168,21 @@ class Control(Token):
         raise mex.exception.ParseError(
                 f"you cannot assign to {self}")
 
+class Internal(Token):
+    """
+    Special tokens which are part of mex's infrastructure.
+
+    Unlike most tokens, these are callables. Expanders
+    call them when they see them.
+    """
+
+    def __init__(self):
+        self.name = self.__class__.__name__
+        self.category = self.INTERNAL
+        self.ch = ''
+
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError()
 
 if __name__=='__main__':
 

mex/state.py
@@ -75,6 +75,29 @@ class _Ifdepth_List(list):
         result = ''.join([_repr(v) for v in self])
         return result
 
+class Callframe:
+    """
+    Description of a macro call.
+
+    Only used for tracebacks; the macros take care of themselves.
+
+    Stored in the list State.call_stack.
+    """
+    def __init__(self,
+            callee,
+            args,
+            location,
+            ):
+        self.callee = callee
+        self.args = args
+        self.location = location
+
+    def __repr__(self):
+        args = ','.join([
+            ''.join([c.ch for c in v])
+            for (f,v) in sorted(self.args.items())])
+        return f'{self.callee}({args}):{self.location}'
+
 class State:
 
     mode_handlers = mex.mode.handlers()
@@ -96,6 +119,7 @@ class State:
         self.parshape = None
 
         self.ifdepth = _Ifdepth_List([True])
+        self.call_stack = []
 
         self.font = None
         self.mode = None

test/control/test_macro.py
@@ -726,9 +726,14 @@ def test_noexpand():
 
     assert ''.join([
         repr(x) for x in state['c'].definition
+        if not x.category==x.INTERNAL
         ])==r'[1][B][2]\b[3][B]'
 
 def _ifcat(q, state):
+    r"""
+    Runs \ifcat<q> on the given State.
+    Returns T if it finds True and F if it finds False.
+    """
     return run_code(
             r"\ifcat " + q +
             r"T\else F\fi",
@@ -754,11 +759,18 @@ def test_conditional_ifcat_p209():
             find='chars',
             state=state)
 
+    # So at this point, [ and ] are both active characters,
+    # and [ has been defined to expand to *.
+
     assert _ifcat(r"\noexpand[\noexpand]", state)=="T"
     assert _ifcat(r"[*", state)=="T"
     assert _ifcat(r"\noexpand[*", state)=="F"
 
 def _ifproper(q, state):
+    r"""
+    Runs \if <q> on the given State.
+    Returns T if it finds True and F if it finds False.
+    """
     return run_code(
             r"\if " + q +
             r" T\else F\fi",
@@ -855,7 +867,13 @@ def test_register_table_name_in_message(capsys):
     roe = capsys.readouterr()
     assert roe.err == roe.out == ''
 
-def test_expansion_with_fewer_params():
+def test_expansion_with_fewer_args():
+    r"""
+    This is a test for currying. It's possible for a function A
+    to call another function B, but supply fewer arguments than
+    it needs. In that case, the remaining arguments are picked
+    up from the text after the call of A.
+    """
     string = (
             r"\def\friendly #1#2#3{#1 #2 my #3 friend}"
             r"\def\greet #1{#1 {Hello} {there}}"

commit 73fc877ec5ac4db71197b18c5e3c323bf8277598

test.run_code() ignores Internal tokens in comparisons
This fixes a problem introduced in commit 705dfb0b two days ago.
All tests now pass.

test/__init__.py
@@ -94,6 +94,10 @@ def run_code(
                 )
 
         for item in e:
+            if isinstance(item, mex.parse.Token) and \
+                    item.category==item.INTERNAL:
+                continue
+
             state.mode.handle(
                     item = item,
                     tokens = e,
@@ -115,6 +119,9 @@ def run_code(
     for item in e:
         general_logger.debug("run_code saw: %s",
                 item)
+
+        if isinstance(item, mex.parse.Token) and item.category==item.INTERNAL:
+            continue
 
         saw.append(item)
 