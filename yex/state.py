b638a506d60803a4c31173d4cdcac858aa8f2179
Marnanel Thurman <marnanel@thurman.org.uk>
2022-03-22 17:41:08 +0000

    lots of documentation for yex.state

--- yex/state.py
+++ yex/state.py
@@ -1,3 +1,7 @@
+# yex/state.py
+
+r"`State` holds the state of a document while it's being processed."
+
 import datetime
 import yex.value
 import yex.box
@@ -18,6 +22,77 @@ ASSIGNMENT_LOG_RECORD = "%s %-8s = %s"
 KEYWORD_WITH_INDEX = re.compile(r'^([^;]+?);?(-?[0-9]+)$')
 
 class State:
+    r"""The state of a document while it's being processed.
+
+    All macro definitions, fonts, and so on are kept here.
+
+    Mostly, you interact with a State as if it was a dict, by getting
+    and setting the values of its elements (known as "subscripting").
+    This makes it clearer and easier when we have to reset values
+    at the end of a TeX group.
+
+    The names of all elements are strings. The values depend on the element.
+    Some possible names:
+
+        - The name of any predefined control word.
+            For example, ``s['if']``. Don't include the backslash prefix.
+        - The name of any user-defined macro.
+        - The name of any register.
+            For example, ``s['count23']`` or ``s['box12']``.
+        - The prefix of any register, such as ``s['count']``
+            You must supply `tokens`, so we can find the rest of it.
+        - Some internal special values:
+            - ``s['_font']``, for the current font.
+            - ``s['_mode']``, for the current mode.
+            - ``s['_output']``, for the current output driver.
+        - A few controls can themselves be subscripted.
+            Writing ``s['font3']`` is equivalent to writing
+            ``s['font'][3]``.
+
+            The second subscript must be an integer,
+            and can be negative. You can also separate the field name
+            from the field subscript with a semicolon. So
+            ``s['font;3']``, ``s['font3']``, and ``s['font'][3]``
+            are equivalant. ``s['cmr10;3']`` couldn't be written
+            without the semicolon.
+
+    Attributes:
+        created_at (`datetime.datetime`): when the State was
+            constructed. This provides initial values for
+            TeX's time-based parameters, such as ``\year``.
+        controls (:obj:`ControlsTable`): all the controls defined,
+            both built-in and user-defined. Registers
+            are stored in the ``registers`` attribute, not here.
+            This may change ([#6](https://gitlab.com/marnanel/yex/-/issues/6)).
+        registers (:obj:`RegisterTable`): the state of all the
+            registers, such as ``\count12``.
+        groups (list of :obj:`Group`): the nested groups
+            of the TeX source being processed, which are
+            created either by ``{``/``}`` or by
+            ``\begingroup``/``\endgroup``.
+        fonts (dict of :obj:`Font`): fonts currently loaded.
+            They need not have identifiers in the controls
+            table, but they're not accessible from TeX code
+            unless they do.
+        font (:obj:`Font`): the currently selected font.
+        mode (:obj:`Mode`): the currently selected mode.
+        output (:obj:`Output`): the output driver. For example,
+            the PDF driver or the SVG driver.
+        next_assignment_is_global (bool): if True, the next
+            use of `__setitem__` will apply until further notice.
+            Otherwise, it applies until the end of the
+            current group.
+        parshape (list of :obj:`Dimen`): you probably don't
+            need to look at this. It's a list of constraints on lengths
+            of lines in the current paragraph, set by ``\parshape``
+            but kept here so it persists.
+        ifdepth (`_Ifdepth_List`): essentially a list of booleans,
+            representing whether particular conditional clauses are
+            executing. For example, after ``\iftrue`` the top member
+            will be True, after ``\iffalse`` it will be False, and
+            ``\else`` will (generally) negate the top member.
+    """
+
 
     mode_handlers = yex.mode.handlers()
 
@@ -46,6 +121,21 @@ class State:
 
     def open(self, what,
             **kwargs):
+
+        r"""Opens a string, a list of characters, or a file for reading.
+
+            Constructs a :obj:`Tokeniser` on `what`,
+            and an :obj:`Expander` on that `Tokeniser`.
+            Returns the `Expander`.
+
+            Args:
+                what (`str`, `list`, or file-like): where we're getting the
+                    symbols from.
+                **kwargs: Arguments to pass to the `Expander`.
+
+            Returns:
+                An :obj:`Expander`.
+            """
         t = yex.parse.Tokeniser(
                 state = self,
                 source = what,
@@ -58,6 +148,27 @@ class State:
 
     def __setitem__(self, field, value,
             from_restore = False):
+        r"""Assigns a value to an element of this state.
+
+            Args:
+                field (`str`): the name of the element to change.
+                    See the class description for a list of field names.
+                value (any): the value to give the element.
+                    Acceptable types and values depend on the field name.
+                from_restore (`bool`): if True, we're in the process of
+                    restoring settings at the end of a group; otherwise,
+                    we're not, and we store a record of this assignment
+                    until we are. You probably don't need to use this.
+
+            Raises:
+                `KeyError`: if the field doesn't name an element
+                `TypeError`: if the value has the wrong type for the field
+                `ValueError`: if there's something wrong with the value
+                and many other possibilities, depending on which element it is
+
+            Returns:
+                `None`
+            """
 
         if field.startswith('_'):
             return self._setitem_internal(field, value, from_restore)
@@ -103,6 +214,30 @@ class State:
             tokens=None,
             the_object_itself=True,
             ):
+        r"""
+        Retrieves the value of an element of this state.
+
+        Args:
+            field (`str`): the name of the element to find.
+                See the class description for a list of field names.
+            tokens (`Expander`): in some cases, `field` may only be a
+                prefix of a proper element name. For example, the count
+                register numbered 23 is named "count23", but this name
+                is three tokens if you write it in TeX: ``\count``, ``2``,
+                and ``3``. The lookup will only fetch ``\count``, which
+                isn't in itself the name of an element. So, in such cases
+                we fetch the next tokens to find the full name.
+            the_object_itself (`bool`): obsolete, and to be deleted soon
+                ([#26](https://gitlab.com/marnanel/yex/-/issues/26)).
+
+        Returns:
+            the value you asked for
+
+        Raises:
+            `KeyError`: if there is no element with the name you requested.
+            `ParseError`: if we attempted to complete the field name with
+                `tokens`, but failed.
+        """
 
         if field.startswith('_'):
             return self._getitem_internal(field, tokens)
@@ -157,6 +292,16 @@ class State:
     def get(self, field, default=None,
             the_object_itself=True,
             tokens=None):
+        r"""
+        Retrieves the value of an element of this state.
+
+        Just like `__getitem__`, except that we return `None` if
+        the lookup fails.
+
+        Args:
+            `default`: what to return if there is no such element.
+                Otherwise, as for `__getitem__`.
+        """
         try:
             return self.__getitem__(field,
                     tokens=tokens,
@@ -228,6 +373,15 @@ class State:
             raise KeyError(field)
 
     def begin_group(self):
+        r"""
+        Opens a new group. Called by ``{`` and ``\begingroup``.
+
+        Args:
+            none
+
+        Returns:
+            `None`
+        """
         new_group = Group(
                 state = self,
                 )
@@ -237,6 +391,19 @@ class State:
                 self.groups)
 
     def end_group(self):
+        r"""
+        Closes a group. Discards all settings made since the most recent
+        `begin_group`, other than global settings.
+        Called by ``}`` and ``\endgroup``.
+
+        Args:
+            none
+
+        Returns:
+            `None`
+        Returns:
+            none
+        """
         if not self.groups:
             raise yex.exception.YexError("More groups ended than began!")
 
@@ -247,20 +414,51 @@ class State:
         ended.run_restores()
 
     def showlists(self):
+        r"""
+        Prints details of the list in the current `Mode`, and of all
+        the containers it contains, and all the containers *they* contain,
+        and so on.
+
+        Implements the `\showlists` debugging command:
+        see p88 of the TeXbook.
+
+        Currently disabled.
+
+        Args:
+            none
+
+        Returns:
+            `None`
         """
-        Implements the \\showlists debugging command.
-        See p88 of the TeXbook.
-        """
-        raise ValueError("showlists...?")
-        a="""
-        for v in reversed(self.values):
-            v['controls']['_mode'].mode.showlist()
-            """
+        raise NotYetImplemented()
 
     def __len__(self):
+        r"""
+        Finds the number of groups currently open, plus 1.
+        (If there are no groups currently open, that works pretty
+        much like a group for most purposes.)
+
+        Returns:
+            The number of groups, plus 1.
+        """
         return len(self.groups)+1
 
     def remember_restore(self, f, v):
+        r"""
+        Stores a record of an assignment, so it can be undone at the end
+        of the current group. Doesn't actually make the assignment.
+        You probably don't want to use this.
+
+        Other than changes to internal flags, this just calls through
+        to `remember_restore` in the topmost :obj:`Group`.
+
+        Args:
+            f: field name
+            v: field value
+
+        Returns:
+            `None`
+        """
         if not self.groups:
             return
         if self.next_assignment_is_global:
@@ -269,12 +467,46 @@ class State:
         self.groups[-1].remember_restore(f,v)
 
 class Group:
+    r"""
+    A group, in the TeX sense.
+
+    Created by ``{`` or ``\begingroup``, and ended by
+    ``}`` or ``\endgroup``.  When the group ends, all assignments
+    (except global assignments) will be undone
+
+    Attributes:
+        state (`State`): the state we're in
+        restores (dict mapping `str` to arbitrary types): element values to
+            restore when the group ends.
+    """
     def __init__(self, state):
         self.state = state
         self.restores = {}
 
     def remember_restore(self, f, v):
+        """
+        Stores `f` and `v` so we can do ``self.state[f]=v`` later.
 
+        If multiple assignments are made to the same element in the
+        same group, we only record the first: that's all we need to know to
+        restore the value, and the others will be inaccurate anyway.
+
+        Ignores ``f="inputlineno"``, since attempting to restore the
+        previous line number would give unexpected results.
+
+        This method is not called "record_restore" because people might
+        interpret "record" as a noun.
+
+        Args:
+            f (`str`): the fieldname of the element
+            v (arbitrary): the value the element had before the assignment
+
+        Raises:
+            None.
+
+        Returns:
+            `None`
+        """
         if f in ('inputlineno', ):
             # that makes no sense
             return
@@ -295,6 +527,17 @@ class Group:
         self.restores[f] = v
 
     def run_restores(self):
+        """
+        Carries out each restore recorded by `remember_restore`.
+
+        The restores happen in no particular order.
+
+        Raises:
+            None.
+
+        Returns:
+            `None`
+        """
         restores_logger.debug("Beginning restores: %s",
                 self.restores)
         self.next_assignment_is_global = False
@@ -332,8 +575,13 @@ class Callframe:
     Description of a macro call.
 
     Only used for tracebacks; the macros take care of themselves.
-
     Stored in the list State.call_stack.
+
+    Attributes:
+        callee (`Token`): the name of the macro that made the call.
+        args (list of lists of `Token`): the arguments to the call.
+        location (`yex.parse.source.Location`): where the call was made
+            (as a named tuple of filename, line, and column).
     """
     def __init__(self,
             callee,
