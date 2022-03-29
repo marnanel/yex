b13c06674b8a24d86d8b3acbba1e5a4b4e5996af
Marnanel Thurman <marnanel@thurman.org.uk>
2022-03-29 20:38:53 +0100

    show_caller: does nothing except that its representation is the name of the caller's caller
    for use in logging, in __init__ methods etc

---
+++ yex/util.py
@@ -0,0 +1,52 @@
+import os
+
+class _ShowCaller:
+    """
+    Singleton class whose representation names the caller of the caller.
+
+    The object of this class does nothing in particular most of the time.
+    But when you use it in the parameters of a logging statement, thus:
+
+    ```
+        logger.debug("ready; was called from %s", show_caller)
+    ```
+
+    it returns a string giving the name, filename, and line number
+    of the caller of the method containing the logging statement.
+
+    If this was done using a function call, the call would take place
+    every time the logging statement was executed, even in production
+    where it would be a no-op.
+
+    Attributes:
+        DEPTH (`int`): how many levels of directory we should consider
+            to be part of our package, rather than being library modules.
+    """
+
+    DEPTH = 1
+
+    def __init__(self):
+        self.prefix = os.path.sep.join(
+                __file__.split(os.path.sep)[:-self.DEPTH]
+        )
+
+    def __repr__(self):
+        import traceback
+
+        stack = reversed(list(enumerate(traceback.extract_stack()[:-1])))
+
+        for i, caller in stack:
+            if caller.filename.startswith(self.prefix):
+                break
+
+        try:
+            i, caller = next(stack)
+            return '%s (%s:%s)' % (
+                    caller.name,
+                    caller.filename[len(self.prefix)+1:],
+                    caller.lineno,
+                    )
+        except StopIteration:
+            return '???'
+
+show_caller = _ShowCaller()
