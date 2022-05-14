49f5d1aea62c314deab2b476bd38f4be80dccea9
Marnanel Thurman <marnanel@thurman.org.uk>
2022-05-14 02:58:17 +0100

    Nullfont.get_character()
    it always raises an exception

--- yex/font/nullfont.py
+++ yex/font/nullfont.py
@@ -15,6 +15,9 @@ class Nullfont(Font):
             def __init__(self):
                 self.dimens = {}
 
+            def get_character(self, n):
+                raise KeyError("nullfont has no characters")
+
         self.metrics = NullfontMetrics()
         self.scale = None
         self.name = 'nullfont'
