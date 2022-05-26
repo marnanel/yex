96e5c69fe5ff20d628b9c234ad20adb14f448c4e
Marnanel Thurman <marnanel@thurman.org.uk>
2022-05-26 14:18:03 +0100

    yex.mode -> yex.mode.mode

---
+++ yex/mode/__init__.py
@@ -0,0 +1,11 @@
+from yex.mode.mode import *
+
+__all__ = [
+        'Mode',
+        'Vertical',
+        'Internal_Vertical',
+        'Horizontal',
+        'Restricted_Horizontal',
+        'Math',
+        'Display_Math',
+        ]
