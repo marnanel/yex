2bc594c140a2c73677c9be4f1179176305fa6438
Marnanel Thurman <marnanel@thurman.org.uk>
2022-08-07 18:20:17 +0100

    test put(target=...)
    Ref issue #99.

--- test/test_put.py
+++ test/test_put.py
@@ -1,6 +1,31 @@
-import yex.put
 import pytest
+import tempfile
+import os
+import glob
+import yex
+from test import *
 
-@pytest.mark.xfail
 def test_put_simple():
-    assert yex.put.put('wombat', catch=False)=='wombat'
+
+    doc = yex()
+
+    assert isinstance(doc, yex.Document)
+
+def test_put_target():
+
+    dirname = tempfile.mkdtemp()
+    target_filename = os.path.join(dirname, 'wombat.html')
+
+    yex.put.put('wombat',
+            target = target_filename,
+            )
+
+    with open(target_filename, 'r') as f:
+        contents = f.read()
+
+    for f in glob.glob(os.path.join(dirname, '*')):
+        os.unlink(f)
+    os.rmdir(dirname)
+
+    assert contents.startswith('<!DOCTYPE html>'), contents
+    assert 'wombat</span>' in contents, contents
