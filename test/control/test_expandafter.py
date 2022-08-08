667eccbf14198486235df09fecda1aa81b89b0f8
Marnanel Thurman <marnanel@thurman.org.uk>
2022-08-08 14:18:10 +0100

    Implementation of \expandafter that doesn't work
    Test added. Ref issue #73.
    
    We currently don't have a way to tell when the second token is done expanding
    but not done executing. For example, if t2 is a macro with three arguments,
    we'd read the name of the macro, then all three arguments (which could be#
    arbitrarily long), then we'd push t1, then we'd expand the macro named in t2.
    
    Fixing this is going to overlap enough with issue #49 that we'll do them together.

---
+++ test/control/test_expandafter.py
@@ -0,0 +1,15 @@
+import yex
+from test import *
+
+def test_expandafter():
+    assert run_code(
+            setup=(
+                r'\def\myis#1#2{My #1 is #2}'
+                r'\def\butunbowed{but unbowed}'
+                ),
+
+            call=(
+                r'\expandafter\butunbowed\myis{head}{bloody}.'
+                ),
+            find='ch',
+            )=='My head is bloody but unbowed.'
