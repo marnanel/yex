c545b77e9a6b95dd7a500bb2938a95a0576c75fd
Marnanel Thurman <marnanel@thurman.org.uk>
2022-08-29 18:09:00 +0100

    tests for IO reading

--- test/test_io.py
+++ test/test_io.py
@@ -27,18 +27,119 @@ def test_io_write_to_terminal(capsys):
 
     assert result==string.replace('\r', '\n')
 
-@pytest.mark.xfail
-def test_io_write_to_file():
+def test_io_write_to_file(fs):
     assert False
 
-@pytest.mark.xfail
-def test_io_write_to_log_but_not_terminal():
+def test_io_write_to_log_but_not_terminal(fs):
     assert False
 
-@pytest.mark.xfail
 def test_io_read_from_terminal():
     assert False
 
-@pytest.mark.xfail
-def test_io_read_from_file():
-    assert False
+TEST_DATA = r"""This contains } some number { of
+matching } lines
+But\par if there is { some set} of } braces
+Then it will { read
+onto the } next line as well"""
+
+def _munge_tokens(t):
+    result = []
+    for thing in t:
+        if isinstance(thing, str):
+            for letter in str:
+                result.append(yex.parse.get_token(c=letter))
+        else:
+            result.append(thing)
+
+    return result
+
+def test_io_read_from_file(fs):
+    with open('fred.tex', 'w') as fred:
+        fred.write(TEST_DATA)
+
+    input_stream = yex.io.InputStream(
+            filename = 'fred.tex',
+            )
+    assert not input_stream.eof
+
+    for expect_eof, line in [
+            (False, ['This contains']),
+            (False, ['matching']),
+            (False, ['But',
+                yex.parse.Control(r'\par'),
+                ' if there is ',
+                yex.parse.Control(r'\par'),
+                ' or ',
+                yex.parse.BeginningGroup(),
+                ' some set',
+                yex.parse.EndGroup(),
+                ' of ']),
+            (False, [' Then it will ',
+                yex.parse.BeginningGroup(),
+                ' read onto the ',
+                yex.parse.EndGroup(),
+                ' next line as well']),
+            (True, [yex.parse.Control(r'\par')]),
+            (True, None),
+            ]:
+
+        expected = _munge_tokens(line)
+
+        found = input_stream.read()
+
+        assert expected==found, line
+        assert input_stream.eof == expect_eof
+
+    input_stream.close()
+    assert input_stream.eof
+
+def test_io_read_from_closed_file(fs):
+    with open('fred.tex', 'w') as fred:
+        fred.write(TEST_DATA)
+
+    input_stream = yex.io.InputStream(
+            filename = 'fred.tex',
+            )
+
+    assert not input_stream.eof
+
+    found = input_stream.read()
+    assert found==_munge_tokens(['This contains'])
+    assert not input_stream.eof
+
+    input_stream.close()
+    assert input_stream.eof
+
+    found = input_stream.read()
+    assert found is None
+
+def test_io_read_from_file_not_found(fs):
+    input_stream = yex.io.InputStream(
+            filename = 'gronda.tex',
+            )
+
+    assert input_stream.eof
+    found = input_stream.read()
+    assert found is None
+
+def test_io_read_supplies_file_extension(fs):
+    with open('wombat.tex', 'w') as f:
+        f.write('wombat')
+    with open('spong.html', 'w') as f:
+        f.write('spong')
+
+    for filename, expected in [
+            ('wombat', 'wombat'),
+            ('wombat.tex', 'wombat'),
+            ('wombat.html', None),
+            ('spong', None),
+            ('spong.html', 'spong'),
+            ]:
+
+        input_stream = yex.io.InputStream(
+                filename = filename,
+                )
+
+        found = input_stream.read()
+        assert found==expected, filename
+        input_stream.close()
