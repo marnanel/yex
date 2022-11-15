f6c4a986de7472212b7bd5aced31422ff3fb16de
Marnanel Thurman <marnanel@thurman.org.uk>
2022-11-15 19:01:46 +0000

    yex.wrap gets its own package

---
+++ yex/wrap/__init__.py
@@ -0,0 +1,10 @@
+from yex.wrap.wrapping import *
+from yex.wrap.fitting import *
+
+__all__ = [
+        'wrap',
+        'VERY_LOOSE',
+        'LOOSE',
+        'DECENT',
+        'TIGHT',
+        ]
