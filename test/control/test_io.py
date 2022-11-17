c8b14aa2dc12bab379d235046d8b9deba1bace84
Marnanel Thurman <marnanel@thurman.org.uk>
2022-11-17 15:52:20 +0000

    missing backslash

--- test/control/test_io.py
+++ test/control/test_io.py
@@ -14,13 +14,13 @@ def test_immediate_write_simple(capsys):
 def test_immediate_write_side_effect(capsys):
     doc = yex.document.Document()
 
-    assert doc['count1']==0
+    assert doc[r'\count1']==0
     assert run_code(
             r"\immediate\write16{Hello \count1=2 world.}",
             find='chars',
             doc=doc,
             )==""
-    assert doc['count1']==2
+    assert doc[r'\count1']==2
 
     result = capsys.readouterr().out
     assert result.strip()=="Hello world."
