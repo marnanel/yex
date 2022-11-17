3468066c6158808a0e73dfca06c0b7476b20d95f
Marnanel Thurman <marnanel@thurman.org.uk>
2022-11-17 20:40:32 +0000

    Check \message as well as \errmessage
    For the plain form of the test, test in favour of a +ve rather
    than against a -ve.

--- test/control/test_message.py
+++ test/control/test_message.py
@@ -17,8 +17,20 @@ def test_errmessage(capsys):
     assert roe.err == "what"
 
 def test_register_table_name_in_params(capsys):
+    run_code(
+            setup=r'\count10=177',
+            call=(
+                r'\def\tenth#1{\the#1\10}'
+                r'\tenth\count'
+                ),
+            find='ch',
+            )
+    roe = capsys.readouterr()
+    assert roe.err == roe.out == ''
+
+def test_register_table_name_in_params_with_errmessage(capsys):
     # Based on ch@ck in plain.tex.
-    # This doesn't parse unless the \errmessage
+    # This doesn't parse unless the \message or \errmessage
     # handler is run, but told not to do anything,
     # even when an if statement would ordinarily stop it.
     #
@@ -26,16 +38,6 @@ def test_register_table_name_in_params(capsys):
     # when it's not executing. That's usually the
     # right answer, but not for \message{} and friends.
 
-    with pytest.raises(yex.exception.ExpectedNumberError):
-        run_code(
-                r"\def\check#1{No room for a new #1}"
-                r"\check\dimen",
-                find='ch',
-                )
-        roe = capsys.readouterr()
-        assert roe.err == roe.out == ''
-
-def test_register_table_name_in_params_with_message(capsys):
     run_code(
             r"\def\check#1{\errmessage{No room for a new #1}}"
             r"\check\dimen",
@@ -44,3 +46,22 @@ def test_register_table_name_in_params_with_message(capsys):
     roe = capsys.readouterr()
     assert roe.err == r'No room for a new \dimen'
     assert roe.out == ''
+
+def test_register_table_name_in_params_with_message(capsys):
+    # Based on ch@ck in plain.tex.
+    # This doesn't parse unless the \message or \errmessage
+    # handler is run, but told not to do anything,
+    # even when an if statement would ordinarily stop it.
+    #
+    # This is because the parser ignores all code
+    # when it's not executing. That's usually the
+    # right answer, but not for \message{} and friends.
+
+    run_code(
+            r"\def\check#1{\message{No room for a new #1}}"
+            r"\check\dimen",
+            find='ch',
+            )
+    roe = capsys.readouterr()
+    assert roe.err == ''
+    assert roe.out == r'No room for a new \dimen'
