116cc3d72c7778b99289478145bee1b6f5c8b150
Marnanel Thurman <marnanel@thurman.org.uk>
2022-11-26 18:37:59 +0000

    look up width of 'x' so we don't need to hardcode it
    also use find='ch'

--- test/control/test_wd.py
+++ test/control/test_wd.py
@@ -2,6 +2,11 @@ import yex
 from test import *
 
 def test_control_wd_querying():
+
+    font = yex.font.Default()
+    width_of_x = font['x'].metrics.width
+
     assert run_code(
             r'\box10=\hbox{x}\dimen20=\wd10(\the\dimen20)',
-            )=='(10pt)'
+            find='ch',
+            )==f'({width_of_x})'
