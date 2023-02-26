7cd362d960d850aef673e57e78c01cd583d39d77
Marnanel Thurman <marnanel@thurman.org.uk>
2023-02-26 19:39:45 +0000

    refactor run_code(find='ch')

--- test/__init__.py
+++ test/__init__.py
@@ -264,13 +264,17 @@ def run_code(
             found)
 
     def get_ch(x):
-        try:
-            return x.ch
-        except AttributeError:
+        for attempt in [
+                lambda x: x.identifier,
+                lambda x: x.ch,
+                lambda x: str(x),
+                ]:
             try:
-                return x.identifier
+                return attempt(x)
             except AttributeError:
-                return str(x)
+                pass
+            except NotImplementedError:
+                pass
 
     def finding(what):
         if what in found:
