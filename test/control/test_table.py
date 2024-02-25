c3c8106f7501aef134c1bec2230f4f7f48006ba7
Marnanel <marnanel@thurman.org.uk>
2024-02-25 00:30:43 +0000

    intermediate

--- test/control/test_table.py
+++ test/control/test_table.py
@@ -8,20 +8,18 @@ def test_controlstable_simple():
     assert len(t.macros_from_styles)==0
     assert t.kwargs['wombat'] == 'banana'
 
-def _populated_table():
-    result = yex.control.ControlsTable()
-    return result
-
 def test_controlstable_set_control():
     t = yex.control.ControlsTable()
-
     fred = yex.control.keyword.Iftrue()
 
     with pytest.raises(KeyError):
         t['fred']
 
     t['fred'] = fred
-    assert t['fred'].name=='iftrue'
+    assert t['fred'] == fred
+    assert t.get('fred') == fred
+    assert t.get('fred', param_control=False) == fred
+    assert t.get('fred', param_control=True) == fred
 
     with pytest.raises(ValueError):
         t['fred'] = 1
@@ -31,12 +29,102 @@ def test_controlstable_set_control():
     with pytest.raises(KeyError):
         t['fred']
 
-def test_controlstable_get():
-    t = _populated_table()
+def test_controlstable_set_parameter():
+    t = yex.control.ControlsTable()
+    jim = yex.control.keyword.Badness()
+
+    with pytest.raises(KeyError):
+        t['jim']
+
+    t['jim'] = jim
+    assert t['jim'] == 0
+    assert t.get('jim') == 0
+    assert t.get('jim', param_control=False) == 0
+    assert t.get('jim', param_control=True) == jim
+
+    assert isinstance(t['jim'], int)
+
+    t['jim'] = 1
+    assert t['jim'] == 1
+
+    t['jim'] = None
+
+    with pytest.raises(KeyError):
+        t['jim']
+
+# FIXME we must still check setitem with dict
+# FIXME we must still check setitem with class
+
+def test_controlstable_len():
+    t = yex.control.ControlsTable()
+    fred = yex.control.keyword.Iftrue()
+    jim = yex.control.keyword.Iffalse()
+
+    assert len(t)==0
+    t['fred'] = fred
+    assert len(t)==1
+    t['jim'] = jim
+    assert len(t)==2
 
 def test_controlstable_keys_values_items_iter():
-    t = _populated_table()
     t = yex.control.ControlsTable()
+    fred = yex.control.keyword.Iftrue()
+    jim = yex.control.keyword.Iffalse()
+    sheila = yex.control.keyword.Ifcase()
+
+    t['fred'] = fred
+    t['jim'] = jim
+    t['sheila'] = sheila
+
+    assert set(t.keys())=={'fred', 'jim', 'sheila'}
+    assert set(t.values())=={fred, jim, sheila}
+    assert set(t.items())=={
+            ('fred', fred),
+            ('jim', jim),
+            ('sheila', sheila),
+            }
+
+    found = set()
+    for f,v in t:
+        found[f] = v
+
+    assert found=={
+            ('fred', fred),
+            ('jim', jim),
+            ('sheila', sheila),
+            }
+
+    assert 'fred' in t
+    assert 'jim' in t
+    assert 'sheila' in t
+    assert 'margery' not in t
 
 def test_controlstable_del():
-    t = _populated_table()
+    t = yex.control.ControlsTable()
+    fred = yex.control.keyword.Iftrue()
+    jim = yex.control.keyword.Iffalse()
+
+    t['fred'] = fred
+    t['jim'] = jim
+
+    assert len(t)==2
+    assert t['fred']==fred
+    assert t['jim']==jim
+
+    del t['fred']
+
+    assert len(t)==1
+    with pytest.raises(KeyError):
+        t['fred']
+    assert t['jim']==jim
+
+    with pytest.raises(KeyError):
+        del t['fred']
+
+    del t['jim']
+
+    assert len(t)==0
+    with pytest.raises(KeyError):
+        t['fred']
+    with pytest.raises(KeyError):
+        t['jim']
