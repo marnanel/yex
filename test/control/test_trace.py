860469fb24ae89c91c5e57f4e74f1530d73b0fd3
Marnanel <marnanel@thurman.org.uk>
2024-10-04 22:28:47 +0100

    capsys changes sys.stdout, so we must refresh it

--- test/control/test_trace.py
+++ test/control/test_trace.py
@@ -1,7 +1,12 @@
 import pytest
 import yex
+import sys
 from test import *
 
 def test_trace_simple(capsys):
+
+    # capsys has changed sys.stdout, so refresh it
+    yex.io.trace.streams = [sys.stdout]
+
     yex.io.trace('octopus')
     assert capsys.readouterr().out == 'octopus\n'
