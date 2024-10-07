fc24663c12636e958a8ebe15cc5995d2cdbda3fb
Marnanel <marnanel@thurman.org.uk>
2024-10-07 19:14:04 +0100

    docstrings
    + catch a few edge cases

--- yex/io/trace.py
+++ yex/io/trace.py
@@ -3,6 +3,18 @@ import sys
 DEFAULT_DEFAULT_LOG_FILENAME = 'yex.log'
 
 class Trace:
+    """
+    Implementation of TeX's tracing system.
+
+    This is a singleton class; you should access it as `yex.io.trace`.
+
+    See also the handlers in `yex.control.keyword.trace`.
+
+    Fields:
+        default_log_filename (str): the filename which will be used
+            if you set `to_file` to `True`.
+        streams (list): open streams to write trace messages to.
+    """
 
     def __init__(self):
         self.streams = [sys.stdout]
@@ -10,9 +22,10 @@ class Trace:
 
     def __call__(self, s):
         """
-        Actually emits a tracing record.
+        Emits a tracing record to all streams in `self.streams`.
 
-        You can monkeypatch this method in testing.
+        Args:
+            s (str): what to output. A newline will be added.
         """
 
         for stream in self.streams:
@@ -22,6 +35,13 @@ class Trace:
 
     @property
     def to_stdout(self):
+        """
+        Whether tracing is going to `sys.stdout` (`bool`).
+
+        This is based on an interpretation of `self.streams`.
+        It uses the value of `sys.stdout` at the time you
+        call it.
+        """
         return sys.stdout in self.streams
 
     @to_stdout.setter
@@ -35,6 +55,23 @@ class Trace:
 
     @property
     def target_file(self):
+        """
+        What stream the tracing is going to, other than `sys.stdout`.
+        Can be None.
+
+        This is based on an interpretation of `self.streams`.
+        It uses the value of `sys.stdout` at the time you
+        call it.
+
+        Setting this field removes all other non-stdout streams
+        from `self.streams`.
+
+        Raises:
+            ValueError: on get, if there are multiple
+                streams in `self.streams` which aren't `sys.stdout`.
+            TypeError: on put, if set to a value which
+                is not file-like.
+        """
         s = [x for x in self.streams if x!=sys.stdout]
 
         if len(s)==0:
@@ -50,17 +87,38 @@ class Trace:
         self.streams = [x for x in self.streams if x==sys.stdout]
 
         if v is not None:
-            assert hasattr(v, 'write'), v
+            if not hasattr(v, 'write'):
+                raise TypeError(v)
             self.streams.append(v)
 
     @property
     def to_file(self):
-        return self.target_file is not None
+        """
+        Whether tracing is going to a stream other than `sys.stdout`
+        (`bool`).
+
+        This is based on an interpretation of `self.streams`.
+        It uses the value of `sys.stdout` at the time you
+        call it.
+
+        If you set this value to `True`, then we open a file
+        called `self.default_log_filename` for writing.
+
+        Raises:
+            TypeError: on put, if set to a value which
+                is not boolean. This is to catch you if you
+                get this field mixed up with `target_file`.
+        """
+        try:
+            return self.target_file is not None
+        except ValueError:
+            return True
 
     @to_file.setter
     def to_file(self, v):
-        assert isinstance(v, bool)
-        if v:
+        if not isinstance(v, bool):
+            raise TypeError(v)
+        elif v:
             self.target_file = open(self.default_log_filename, 'w')
         else:
             self.target_file = None
