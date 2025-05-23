f8499d9176dbff8a6a7a4af5ab4e7b0f29b55d60
Marnanel Thurman <marnanel@marnanel.org>
2025-05-23 16:47:19 +0100

    correct project path

--- docs/conf.py
+++ docs/conf.py
@@ -8,9 +8,8 @@ import os
 import sys
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                            "../yex"))
+                                            ".."))
 sys.path.insert(0, project_root)
-
 
 # -- Project information -----------------------------------------------------
 
