8c959a5a5266507792a4231db3bf01612fc2489e
Marnanel Thurman <marnanel@marnanel.org>
2025-10-27 00:21:53 +0000

    fix case where -v is specified but no loggers are named

--- yex/__main__.py
+++ yex/__main__.py
@@ -94,7 +94,10 @@ def _parse_output_filename(source, output):
 
 def run():
     if args.verbose:
-        args.loggers += ',verbose'
+        if args.loggers is None:
+            args.loggers = 'verbose'
+        else:
+            args.loggers += ',verbose'
 
     yex.logging.selectLoggers(
             handlers = args.loggers,
