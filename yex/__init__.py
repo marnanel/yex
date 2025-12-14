711d6196ce92ff6b912c2bb1e4e8863b6969ea7e
Marnanel Thurman <marnanel@marnanel.org>
2025-12-14 04:46:42 +0000

    the yex package exports only Document by default

--- yex/__init__.py
+++ yex/__init__.py
@@ -5,22 +5,12 @@ __author__ = 'Marnanel Thurman'
 __license__ = 'GPL-2'
 __copyright__ = 'Copyright (c) 2022 Marnanel Thurman'
 
-import sys
-import types
-
 import yex.decorator
 import yex.output
 import yex.control
 import yex.mode
 from yex.document.document import Document
-
-__all__ = [
-        'control',
-        'decorator',
-        'mode',
-        'output',
-        'Document',
-        ]
+from yex.module import make_module_into_yexmodule
 
 def _add_logging_level_trace():
     import logging
@@ -33,12 +23,8 @@ def _add_logging_level_trace():
 
 _add_logging_level_trace()
 
-# Cast ourselves to a subtype. This means we can call yex().
-# See https://stackoverflow.com/questions/1060796/callable-modules .
-class YexModule(types.ModuleType):
-    def __call__(self, *args, **kwargs):
-        import yex.put
+make_module_into_yexmodule(__name__)
 
-        return yex.put.put(*args, **kwargs)
-
-sys.modules[__name__].__class__ = YexModule
+__all__ = [
+        'Document',
+        ]
